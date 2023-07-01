c43295b349ca55049b6833f185330a9fed339306
R1DF <33981824+R1DF@users.noreply.github.com>
2023-07-01 23:34:09 +0600

    Added template class for showcases

---
+++ showcases/showcase_template.py
@@ -0,0 +1,17 @@
+"""This Python module contains the ShowcaseTemplate class that every Showcase class must inherit from.
+
+Classes:
+    ShowcaseTemplate: The parent class for every showcase that serves as a template."""
+
+# Imports
+from tkinter import Tk, Canvas
+
+
+#  Canvas class (must be named as Showcase)
+class ShowcaseTemplate(Canvas):
+    def __init__(self, master: Tk):
+        super().__init__(master)
+
+    def remove(self, widget):
+        if isinstance(widget, int):
+            self.delete(widget)
