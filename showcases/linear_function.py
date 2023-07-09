27a7a841f0d06bef7c0566e5effe6ab3045889db
R1DF <33981824+R1DF@users.noreply.github.com>
2023-07-09 19:04:49 +0600

    Changes to metadata

--- showcases/linear_function.py
+++ showcases/linear_function.py
@@ -1,12 +1,16 @@
 # Imports
 from tkinter import Toplevel, Label, Entry, Button
-from .showcase_template import ShowcaseTemplate
+from showcase_template import ShowcaseTemplate
 import re
-import sys
 
 # Meta
-MODULE_NAME = "Drawing a line with a linear function (y=mx+b)"
+MODULE_NAME = "Drawing a linear function"
 MODULE_AUTHOR = "R1DF"
+MODULE_DESCRIPTION = "This showcase will allow you to enter a linear function and the program will graph it. Only\n" \
+                     "works at a 3:2 width to height resolution."
+MODULE_CONDITIONS = [
+    [{"window_height_ratio": 1.5}, "Window resolution needs to have 3:2 ratio."]
+]
 
 # Function Inputter Toplevel
 class FunctionInputter(Toplevel):
@@ -80,93 +84,89 @@ class Showcase(ShowcaseTemplate):
         super().__init__(master)
 
         # Showcase attributes
-        if self.master.WIDTH / self.master.HEIGHT == 3/2:
-            self.function_line = None
-            self.abscissa_line = self.create_line(
-                0,
-                self.master.HEIGHT // 2,
-                self.master.WIDTH,
-                self.master.HEIGHT // 2
-            )
-            self.ordinate_line = self.create_line(
-                self.master.WIDTH // 2,
-                0,
-                self.master.WIDTH // 2,
-                self.master.HEIGHT
-            )
+        self.function_line = None
+        self.abscissa_line = self.create_line(
+            0,
+            self.master.HEIGHT // 2,
+            self.master.WIDTH,
+            self.master.HEIGHT // 2
+        )
+        self.ordinate_line = self.create_line(
+            self.master.WIDTH // 2,
+            0,
+            self.master.WIDTH // 2,
+            self.master.HEIGHT
+        )
 
-            # Offsets (not TICK offsets) determine how much does the distance between ticks on a plane mean in units of
-            # length (e.g. 1 tick up is 2 units high and 1 tick to the right is 2 units to the right)
-            self.unit_offset = 1
+        # Offsets (not TICK offsets) determine how much does the distance between ticks on a plane mean in units of
+        # length (e.g. 1 tick up is 2 units high and 1 tick to the right is 2 units to the right)
+        self.unit_offset = 1
 
-            # Making calculations to draw "ticks" (small lines that space out coordinate points)
-            tick_offset_1 = self.master.WIDTH // 20   # 20 is constant. Setting the value to anything else breaks everything
-            tick_offset_2 = self.master.HEIGHT // 20
-            self.tick_offset = min(tick_offset_1, tick_offset_2)
-            self.x_ticks_amount = self.master.WIDTH // self.tick_offset
-            self.y_ticks_amount = self.master.HEIGHT // self.tick_offset
+        # Making calculations to draw "ticks" (small lines that space out coordinate points)
+        tick_offset_1 = self.master.WIDTH // 20   # 20 is constant. Setting the value to anything else breaks everything
+        tick_offset_2 = self.master.HEIGHT // 20
+        self.tick_offset = min(tick_offset_1, tick_offset_2)
+        self.x_ticks_amount = self.master.WIDTH // self.tick_offset
+        self.y_ticks_amount = self.master.HEIGHT // self.tick_offset
 
-            self.x_ticks = []
-            self.y_ticks = []
-            self.x_units = []
-            self.y_units = []
+        self.x_ticks = []
+        self.y_ticks = []
+        self.x_units = []
+        self.y_units = []
 
-            # Drawing out ticks from calculations
-            for tick in range(1, self.x_ticks_amount + 1):
-                self.x_ticks.append(self.create_line(
-                    tick * self.tick_offset,
-                    (self.master.HEIGHT // 2) - 5,
-                    tick * self.tick_offset,
-                    (self.master.HEIGHT // 2) + 5
-                ))
+        # Drawing out ticks from calculations
+        for tick in range(1, self.x_ticks_amount + 1):
+            self.x_ticks.append(self.create_line(
+                tick * self.tick_offset,
+                (self.master.HEIGHT // 2) - 5,
+                tick * self.tick_offset,
+                (self.master.HEIGHT // 2) + 5
+            ))
 
-            for tick in range(1, self.y_ticks_amount + 1):
-                self.y_ticks.append(self.create_line(
-                    (self.master.WIDTH // 2) - 5,
-                    tick * self.tick_offset,
-                    (self.master.WIDTH // 2) + 5,
-                    tick * self.tick_offset
-                ))
+        for tick in range(1, self.y_ticks_amount + 1):
+            self.y_ticks.append(self.create_line(
+                (self.master.WIDTH // 2) - 5,
+                tick * self.tick_offset,
+                (self.master.WIDTH // 2) + 5,
+                tick * self.tick_offset
+            ))
 
-            # Drawing out units
-            x_mid_tick_value = len(self.x_ticks) // 2
-            for tick in self.x_ticks:
-                unit_shown = self.unit_offset * ((self.x_ticks.index(tick) + 1) - x_mid_tick_value)
-                if unit_shown == 0:
-                    continue  # No need to draw out 0
+        # Drawing out units
+        x_mid_tick_value = len(self.x_ticks) // 2
+        for tick in self.x_ticks:
+            unit_shown = self.unit_offset * ((self.x_ticks.index(tick) + 1) - x_mid_tick_value)
+            if unit_shown == 0:
+                continue  # No need to draw out 0
 
-                tick_coordinates = self.bbox(tick)
-                self.x_units.append([self.create_text(
-                    tick_coordinates[2] - 2,
-                    (self.master.HEIGHT // 2) + 13,
-                    text=f"{unit_shown}",
-                    font="Arial 8"
-                ), unit_shown])  # [unit object, unit shown]
+            tick_coordinates = self.bbox(tick)
+            self.x_units.append([self.create_text(
+                tick_coordinates[2] - 2,
+                (self.master.HEIGHT // 2) + 13,
+                text=f"{unit_shown}",
+                font="Arial 8"
+            ), unit_shown])  # [unit object, unit shown]
 
-            y_mid_tick_value = len(self.y_ticks) // 2
-            for tick in self.y_ticks:
-                unit_shown = self.unit_offset * (y_mid_tick_value - (self.y_ticks.index(tick) + 1))
-                if unit_shown == 0:
-                    continue  # No need to draw out 0
+        y_mid_tick_value = len(self.y_ticks) // 2
+        for tick in self.y_ticks:
+            unit_shown = self.unit_offset * (y_mid_tick_value - (self.y_ticks.index(tick) + 1))
+            if unit_shown == 0:
+                continue  # No need to draw out 0
 
-                tick_coordinates = self.bbox(tick)
-                self.x_units.append([self.create_text(
-                    (self.master.WIDTH // 2) + 13,
-                    tick_coordinates[3] - 2,
-                    text=f"{unit_shown}",
-                    font="Arial 8"
-                ), unit_shown])  # [unit object, unit shown]
+            tick_coordinates = self.bbox(tick)
+            self.x_units.append([self.create_text(
+                (self.master.WIDTH // 2) + 13,
+                tick_coordinates[3] - 2,
+                text=f"{unit_shown}",
+                font="Arial 8"
+            ), unit_shown])  # [unit object, unit shown]
 
-            # Create toplevel
-            self.function_inputter = FunctionInputter(self, self.master)
-        else:
-            print("Please change your window resolution to fit the ratio 3:2 to make the showcase work.")
-            sys.exit()
+        # Create toplevel
+        self.function_inputter = FunctionInputter(self, self.master)
 
     def get_relative_coordinates(self, coordinates):
         # Calculating Cartesian coordinates with account to unit offset
         cartesian_x, cartesian_y = [coordinate / self.unit_offset for coordinate in coordinates]
-        cartesian_y = -cartesian_y  #  flipped to negative IDK how but this actually works? TODO figure out why this works
+        cartesian_y = -cartesian_y
 
         x_additive = self.x_ticks_amount / 2  # Additives remove negatives on the number line
         y_additive = self.y_ticks_amount / 2
