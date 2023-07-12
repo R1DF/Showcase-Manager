3a165ebb0ebb6b4c9dc81b937840d6f9fac1fa9e
R1DF <33981824+R1DF@users.noreply.github.com>
2023-07-12 13:56:04 +0600

    Completed showcase

--- showcases/free_body_diagram.py
+++ showcases/free_body_diagram.py
@@ -9,6 +9,10 @@ MODULE_AUTHOR = "R1DF"
 MODULE_DESCRIPTION = "This showcase allows the user to control a free body diagram of an object and add forces\n" \
                      "to it, making some calculations as a result."
 
+# Float checker:
+def is_float(text: str):
+    return text.isnumeric() or (text.count(".") == 1 and text.replace(".", "").isnumeric())
+
 # Box mass setter
 class BoxMassSetterToplevel(Toplevel):
     def __init__(self, master, master_showcase):
@@ -28,15 +32,23 @@ class BoxMassSetterToplevel(Toplevel):
         self.set_button = Button(self, text="Done", command=self.set_mass)
         self.set_button.pack()
 
+        self.accuracy_label = Label(self, text=f"Rounds to {self.master_showcase.QUANTITY_PRECISION_CONSTANT} d.p.")
+        self.accuracy_label.pack()
+
         # Protocol
         self.protocol("WM_DELETE_WINDOW", self.handle_close)
 
     def set_mass(self):
         mass_str = self.mass_entry.get().strip()
-        if not mass_str.isnumeric():
+        if not is_float(mass_str):
             messagebox.showerror("Error", "Please enter a numeric value for the mass.")
             return
-        self.master_showcase.configure_mass(int(mass_str))
+
+        mass = round(float(mass_str), self.master_showcase.QUANTITY_PRECISION_CONSTANT)
+        if mass == 0:
+            messagebox.showerror("Error", "Mass cannot be 0kg.")
+            return
+        self.master_showcase.configure_mass(float(mass_str))
         self.handle_close()
 
     def handle_close(self):
@@ -67,7 +79,8 @@ class ForceAdderToplevel(Toplevel):
             self.directions.append("Left")
 
         # Setting up widgets
-        self.introduction_label = Label(self, text="Select force details:")
+        self.introduction_label = Label(self, text=f"Select force details [rounds to "
+                                                   f"{self.master_showcase.QUANTITY_PRECISION_CONSTANT} d.p.]:")
         self.introduction_label.pack()
 
         self.details_frame = Frame(self)
@@ -105,12 +118,12 @@ class ForceAdderToplevel(Toplevel):
             messagebox.showerror("Error", "Please select a direction.")
             return
 
-        if not magnitude_str.isnumeric():
+        if not is_float(magnitude_str):
             messagebox.showerror("Error", "Please enter a numerical value for the force.")
             return
 
-        magnitude = int(magnitude_str)
-        if not magnitude:
+        magnitude = round(float(magnitude_str), self.master_showcase.QUANTITY_PRECISION_CONSTANT)
+        if magnitude == 0:
             messagebox.showerror("Error", "Force cannot be 0N.")
             return
 
@@ -126,6 +139,7 @@ class Showcase(ShowcaseTemplate):
         # Showcase attributes
         self.BOX_WIDTH = 300
         self.BOX_HEIGHT = 100
+        self.QUANTITY_PRECISION_CONSTANT = 3
         self.object = self.create_rectangle(
             (self.master.WIDTH - self.BOX_WIDTH) // 2,
             (self.master.HEIGHT - self.BOX_HEIGHT) // 2,
@@ -174,6 +188,19 @@ class Showcase(ShowcaseTemplate):
                 text="Set Box Mass"
             )
         }
+        self.create_text(self.master.WIDTH // 2, self.master.HEIGHT - 20, text="Arrows are not to scale.")
+        self.net_force_text = self.create_text(
+            95,
+            self.master.HEIGHT - 70,
+            text="",
+            justify="center"
+        )
+        self.acceleration_text = self.create_text(
+            self.master.WIDTH - 95,
+            self.master.HEIGHT - 70,
+            text="",
+            justify="center"
+        )
 
         # Bindings
         self.bind("<Motion>", self.handle_cursor_move, add="+")
@@ -195,7 +222,7 @@ class Showcase(ShowcaseTemplate):
         add_force_box_coordinates = self.bbox(self.add_force_box["rect"])
         mass_setter_box_coordinates = self.bbox(self.set_box_mass_box["rect"])
         if add_force_box_coordinates[0] <= event.x <= add_force_box_coordinates[2] and add_force_box_coordinates[1] <= event.y <= add_force_box_coordinates[3]:
-            if self.force_adder_toplevel is None and any([len(x) < 4 for x in self.forces.values()]):
+            if self.force_adder_toplevel is None and any([len(x) < 3 for x in self.forces.values()]):
                 self.force_adder_toplevel = ForceAdderToplevel(self.master, self)
         elif mass_setter_box_coordinates[0] <= event.x <= mass_setter_box_coordinates[2] and mass_setter_box_coordinates[1] <= event.y <= mass_setter_box_coordinates[3]:
             if self.box_mass_setter_toplevel is None:
@@ -203,6 +230,7 @@ class Showcase(ShowcaseTemplate):
 
     def add_force(self, direction, magnitude):
         # Adding force magnitude
+        magnitude = round(magnitude, self.QUANTITY_PRECISION_CONSTANT)
         self.forces[direction].append(magnitude)
         offset_multiple_index = len(self.forces[direction]) - 1
         object_coordinates = self.bbox(self.object)
@@ -269,10 +297,46 @@ class Showcase(ShowcaseTemplate):
                     text=f"{magnitude}N"
                 ))
 
+        # Updating calculations
+        self.make_calculations()
+
     def configure_mass(self, mass):
-        self.object_mass = mass
+        self.object_mass = round(mass, self.QUANTITY_PRECISION_CONSTANT)
         self.itemconfig(self.mass_label, text=f"{mass}kg")
         self.make_calculations()
 
     def make_calculations(self):
-        pass
+        # Calculating net forces
+        net_forces = []  # Up +, Down -, Right +, Left -
+        for force_list in self.forces.values():
+            net_forces.append(sum(force_list))
+
+        if not any(net_forces):  # If every force value is empty
+            return
+
+        net_horizontal_force = round(net_forces[2] - net_forces[3], self.QUANTITY_PRECISION_CONSTANT)
+        net_vertical_force = round(net_forces[0] - net_forces[1], self.QUANTITY_PRECISION_CONSTANT)
+
+        # Updating forces label
+        net_force_text = "Net force:\n"
+        if net_horizontal_force:
+            net_force_text += f"{abs(net_horizontal_force)}N {'right' if net_horizontal_force > 0 else 'left'}\n"
+        if net_vertical_force:
+            net_force_text += f"{abs(net_vertical_force)}N {'up' if net_vertical_force > 0 else 'down'}"
+        if (not net_horizontal_force) and (not net_vertical_force):
+            net_force_text += "The resultant force is 0N\nin each direction."
+        self.itemconfig(self.net_force_text, text=net_force_text)
+
+        # Checking for mass to make acceleration
+        if self.object_mass is not None:
+            acceleration_text = "Acceleration:\n"
+            horizontal_acceleration = round(net_horizontal_force / self.object_mass, self.QUANTITY_PRECISION_CONSTANT)
+            vertical_acceleration = round(net_vertical_force / self.object_mass, self.QUANTITY_PRECISION_CONSTANT)
+            if horizontal_acceleration:
+                acceleration_text += f"{abs(horizontal_acceleration)}m/s^2 {'right' if horizontal_acceleration > 0 else 'left'}\n"
+            if vertical_acceleration:
+                acceleration_text += f"{abs(vertical_acceleration)}m/s^2 {'up' if vertical_acceleration > 0 else 'down'}"
+            if (not horizontal_acceleration) and (not vertical_acceleration):
+                acceleration_text += "There is no acceleration."
+            self.itemconfig(self.acceleration_text, text=acceleration_text)
+
